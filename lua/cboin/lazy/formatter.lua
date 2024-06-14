6ea69569238161ac992872f715dc4b4ec71819df
Christian Boin <cboin@amazon.com>
2024-06-14 11:10:45 -0700

    feat: add formatting and lint

---
+++ lua/cboin/lazy/formatter.lua
@@ -0,0 +1,21 @@
+return {
+        "stevearc/conform.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+                local conform = require("conform")
+
+                conform.setup({
+                        formatters_by_ft = {
+                                lua = { "stylua" },
+                                markdown = { "markdownlint" }
+                        },
+                })
+
+
+                vim.keymap.set({ "n", "v" }, "<Leader>vf", function()
+                        conform.format({
+                                lsp_fallback = true, async = false, timeout_ms = 500
+                        })
+                end)
+        end
+}
