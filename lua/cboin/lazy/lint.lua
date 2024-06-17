afbe0ec3aea66933b0fecb6c4dd46a7354ed1e02
Christian Boin <cboin@amazon.com>
2024-06-17 16:42:43 -0400

    feat: ignore errors so uninstalled linters don't cause issues by default

--- lua/cboin/lazy/lint.lua
+++ lua/cboin/lazy/lint.lua
@@ -18,11 +18,11 @@ return {
         vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
             group = lint_augroup,
             callback = function()
-                lint.try_lint()
+                lint.try_lint(nil, {ignore_errors = true})
             end
         })
         vim.keymap.set("n", "<Leader>vl", function()
-            lint.try_lint()
+            lint.try_lint(nil, {ignore_errors = true})
         end)
     end
 }
