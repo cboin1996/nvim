c2510813e21102bc3439ffab0cc05735afbc3ed6
Christian Boin <cboin@amazon.com>
2024-08-28 08:32:01 -0700

    feat: add file previewing function and markdown previewing

--- lua/cboin/lazy/previewers.lua
+++ lua/cboin/lazy/previewers.lua
@@ -1,7 +1,14 @@
+function PreviewCommand(command)
+    vim.keymap.set("n", "<leader>pp", command, { desc = "toggle preview" })
+end
+
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
+    config = function()
+        PreviewCommand(":MarkdownPreviewToggle<CR>")
+    end
 }
 
